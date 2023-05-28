5ad0800b6c69712729b6bafcd997dbe6e775394d
TobiasHafner <t.hafner@stud.unibas.ch>
2023-05-28 13:32:32 +0200

    add driver registration using classname

--- src/main/java/org/polypheny/jdbc/PolyphenyDriver.java
+++ src/main/java/org/polypheny/jdbc/PolyphenyDriver.java
@@ -1,6 +1,7 @@
 package org.polypheny.jdbc;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.DriverPropertyInfo;
 import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
@@ -8,6 +9,7 @@ import java.util.Properties;
 import java.util.logging.Logger;
 
 public class PolyphenyDriver implements java.sql.Driver {
+
     public static final String DRIVER_URL_SCHEMA = "jdbc:polypheny:";
     public static final String DEFAULT_HOST = "localhost";
     public static final int DEFAULT_PORT = 20591;
@@ -21,51 +23,71 @@ public class PolyphenyDriver implements java.sql.Driver {
     public static final String PROPERTY_PASSWORD_KEY = "password";
     public static final String PROPERTY_NAMESPACE_KEY = "namespace";
 
-    /*
-     * Connection urls ar of the form jdbc:polypheny://host:port/namespace?key1=value1&key2=value2
-     */
+
+    static {
+        new PolyphenyDriver().register();
+    }
+
+
+    private void register() {
+        try {
+            DriverManager.registerDriver( this );
+        } catch ( SQLException e ) {
+            System.out.println(
+                    "Error occurred while registering JDBC driver "
+                            + this + ": " + e.toString() );
+        }
+    }
+
+
     @Override
-    public Connection connect(String url, Properties properties) throws SQLException {
-        if (!acceptsURL(url)) {
+    public Connection connect( String url, Properties properties ) throws SQLException {
+        if ( !acceptsURL( url ) ) {
             return null;
         }
-        ConnectionString connectionString = new ConnectionString(url, properties);
-        ProtoInterfaceClient protoInterfaceClient = new ProtoInterfaceClient(connectionString.getTarget());
-        protoInterfaceClient.connect(connectionString.getParameters());
-        return new PolyphenyConnection(protoInterfaceClient);
+        ConnectionString connectionString = new ConnectionString( url, properties );
+        ProtoInterfaceClient protoInterfaceClient = new ProtoInterfaceClient( connectionString.getTarget() );
+        protoInterfaceClient.connect( connectionString.getParameters() );
+        return new PolyphenyConnection( protoInterfaceClient );
     }
 
+
     @Override
-    public boolean acceptsURL(String url) throws SQLException {
-        if (url == null) {
-            throw new SQLException("URL must no be null.");
+    public boolean acceptsURL( String url ) throws SQLException {
+        if ( url == null ) {
+            throw new SQLException( "URL must no be null." );
         }
-        return url.startsWith(DRIVER_URL_SCHEMA);
+        return url.startsWith( DRIVER_URL_SCHEMA );
     }
 
 
     @Override
-    public DriverPropertyInfo[] getPropertyInfo(String s, Properties properties) throws SQLException {
+    public DriverPropertyInfo[] getPropertyInfo( String s, Properties properties ) throws SQLException {
         return new DriverPropertyInfo[0];
     }
+
 
     @Override
     public int getMajorVersion() {
         return 0;
     }
 
+
     @Override
     public int getMinorVersion() {
         return 0;
     }
+
 
     @Override
     public boolean jdbcCompliant() {
         return false;
     }
 
+
     @Override
     public Logger getParentLogger() throws SQLFeatureNotSupportedException {
         return null;
     }
+
 }
