053abd39857b9033cfa21cbbaa1da4f2d1414894
TobiasHafner <t.hafner@stud.unibas.ch>
2023-05-30 14:43:27 +0200

    add compiled protos

--- src/main/java/org/polypheny/jdbc/ProtoInterfaceClient.java
+++ src/main/java/org/polypheny/jdbc/ProtoInterfaceClient.java
@@ -9,48 +9,53 @@ import java.util.Map;
 import java.util.UUID;
 
 public class ProtoInterfaceClient {
+
     private static final int MAJOR_API_VERSION = 2;
     private static final int MINOR_API_VERSION = 0;
     private final ProtoInterfaceGrpc.ProtoInterfaceBlockingStub blockingStub;
     private final ProtoInterfaceGrpc.ProtoInterfaceStub asyncStub;
     private final String clientUUID;
 
-    public ProtoInterfaceClient(String target) {
+
+    public ProtoInterfaceClient( String target ) {
         this.clientUUID = UUID.randomUUID().toString();
-        ClientMetaInterceptor interceptor = new ClientMetaInterceptor(clientUUID);
-        Channel channel = Grpc.newChannelBuilder(target, InsecureChannelCredentials.create())
-                .intercept(interceptor)
+        Channel channel = Grpc.newChannelBuilder( target, InsecureChannelCredentials.create() )
+                .intercept( new ClientMetaInterceptor( clientUUID ) )
                 .build();
-        this.blockingStub = ProtoInterfaceGrpc.newBlockingStub(channel);
-        this.asyncStub = ProtoInterfaceGrpc.newStub(channel);
+        this.blockingStub = ProtoInterfaceGrpc.newBlockingStub( channel );
+        this.asyncStub = ProtoInterfaceGrpc.newStub( channel );
 
     }
 
-    public void connect(Map<String, String> properties) {
+
+    public void connect( Map<String, String> properties ) {
         ConnectionRequest connectionRequest = ConnectionRequest.newBuilder()
-                .setMajorApiVersion(MAJOR_API_VERSION)
-                .setMinorApiVersion(MINOR_API_VERSION)
-                .setClientUUID(clientUUID)
-                .putAllConnectionProperties(properties)
+                .setMajorApiVersion( MAJOR_API_VERSION )
+                .setMinorApiVersion( MINOR_API_VERSION )
+                .setClientUUID( clientUUID )
+                .putAllConnectionProperties( properties )
                 .build();
-        ConnectionReply connectionReply = blockingStub.connect(connectionRequest);
-        if (!connectionReply.getIsCompatible()) {
-            throw new ProtoInterfaceServiceException("client version " + getClientApiVersionString()
-                    + "not compatible with server version " + getServerApiVersionString(connectionReply) + ".");
+        ConnectionReply connectionReply = blockingStub.connect( connectionRequest );
+        if ( !connectionReply.getIsCompatible() ) {
+            throw new ProtoInterfaceServiceException( "client version " + getClientApiVersionString()
+                    + "not compatible with server version " + getServerApiVersionString( connectionReply ) + "." );
         }
     }
 
-    public QueryResult executeSimpleQuery(String sql) {
+
+    public QueryResult executeSimpleQuery( String sql ) {
         SimpleSqlQuery query = SimpleSqlQuery.newBuilder()
-                .setQuery(sql)
+                .setQuery( sql )
                 .build();
-        return blockingStub.executeSimpleSqlQuery(query);
+        return blockingStub.executeSimpleSqlQuery( query );
     }
 
-    private String getServerApiVersionString(ConnectionReply connectionReply) {
+
+    private String getServerApiVersionString( ConnectionReply connectionReply ) {
         return connectionReply.getMajorApiVersion() + "." + connectionReply.getMinorApiVersion();
 
     }
+
 
     private String getClientApiVersionString() {
         return MAJOR_API_VERSION + "." + MINOR_API_VERSION;
