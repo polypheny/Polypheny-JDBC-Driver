837faaa6e541234e1515b35911585cf6b1c49508
TobiasHafner <t.hafner@stud.unibas.ch>
2023-05-30 15:07:26 +0200

    add not implemented exceptions

--- src/main/java/org/polypheny/jdbc/PolyphenyConnection.java
+++ src/main/java/org/polypheny/jdbc/PolyphenyConnection.java
@@ -4,6 +4,7 @@ import java.sql.*;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.Executor;
+import lombok.SneakyThrows;
 
 public class PolyphenyConnection implements Connection {
     private ProtoInterfaceClient protoInterfaceClient;
@@ -22,266 +23,268 @@ public class PolyphenyConnection implements Connection {
 
     @Override
     public PreparedStatement prepareStatement(String sql) throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public CallableStatement prepareCall(String s) throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public String nativeSQL(String s) throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void setAutoCommit(boolean b) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public boolean getAutoCommit() throws SQLException {
-        return false;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void commit() throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void rollback() throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void close() throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public boolean isClosed() throws SQLException {
-        return false;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public DatabaseMetaData getMetaData() throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void setReadOnly(boolean b) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public boolean isReadOnly() throws SQLException {
-        return false;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void setCatalog(String s) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public String getCatalog() throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void setTransactionIsolation(int i) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public int getTransactionIsolation() throws SQLException {
-        return 0;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public SQLWarning getWarnings() throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void clearWarnings() throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public Statement createStatement(int i, int i1) throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public PreparedStatement prepareStatement(String s, int i, int i1) throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public CallableStatement prepareCall(String s, int i, int i1) throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public Map<String, Class<?>> getTypeMap() throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void setTypeMap(Map<String, Class<?>> map) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void setHoldability(int i) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public int getHoldability() throws SQLException {
-        return 0;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public Savepoint setSavepoint() throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public Savepoint setSavepoint(String s) throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void rollback(Savepoint savepoint) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void releaseSavepoint(Savepoint savepoint) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public Statement createStatement(int i, int i1, int i2) throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public PreparedStatement prepareStatement(String s, int i, int i1, int i2) throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public CallableStatement prepareCall(String s, int i, int i1, int i2) throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public PreparedStatement prepareStatement(String s, int i) throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public PreparedStatement prepareStatement(String s, int[] ints) throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public PreparedStatement prepareStatement(String s, String[] strings) throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public Clob createClob() throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public Blob createBlob() throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public NClob createNClob() throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public SQLXML createSQLXML() throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public boolean isValid(int i) throws SQLException {
-        return false;
+        throw new SQLException("Feature not implemented");
     }
 
+    @SneakyThrows
     @Override
     public void setClientInfo(String s, String s1) throws SQLClientInfoException {
-
+        throw new SQLException("Feature not implemented");
     }
 
+    @SneakyThrows
     @Override
     public void setClientInfo(Properties properties) throws SQLClientInfoException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public String getClientInfo(String s) throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public Properties getClientInfo() throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public Array createArrayOf(String s, Object[] objects) throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public Struct createStruct(String s, Object[] objects) throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void setSchema(String s) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public String getSchema() throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void abort(Executor executor) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void setNetworkTimeout(Executor executor, int i) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public int getNetworkTimeout() throws SQLException {
-        return 0;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public <T> T unwrap(Class<T> aClass) throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public boolean isWrapperFor(Class<?> aClass) throws SQLException {
-        return false;
+        throw new SQLException("Feature not implemented");
     }
 }
--- src/main/java/org/polypheny/jdbc/PolyphenyStatement.java
+++ src/main/java/org/polypheny/jdbc/PolyphenyStatement.java
@@ -30,127 +30,127 @@ public class PolyphenyStatement implements Statement {
 
     @Override
     public void close() throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public int getMaxFieldSize() throws SQLException {
-        return 0;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void setMaxFieldSize(int i) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public int getMaxRows() throws SQLException {
-        return 0;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void setMaxRows(int i) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void setEscapeProcessing(boolean b) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public int getQueryTimeout() throws SQLException {
-        return 0;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void setQueryTimeout(int i) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void cancel() throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public SQLWarning getWarnings() throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void clearWarnings() throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void setCursorName(String s) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public boolean execute(String s) throws SQLException {
-        return false;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public ResultSet getResultSet() throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public int getUpdateCount() throws SQLException {
-        return 0;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public boolean getMoreResults() throws SQLException {
-        return false;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void setFetchDirection(int i) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public int getFetchDirection() throws SQLException {
-        return 0;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void setFetchSize(int i) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public int getFetchSize() throws SQLException {
-        return 0;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public int getResultSetConcurrency() throws SQLException {
-        return 0;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public int getResultSetType() throws SQLException {
-        return 0;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void addBatch(String s) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void clearBatch() throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public int[] executeBatch() throws SQLException {
-        return new int[0];
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
@@ -160,81 +160,81 @@ public class PolyphenyStatement implements Statement {
 
     @Override
     public boolean getMoreResults(int i) throws SQLException {
-        return false;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public ResultSet getGeneratedKeys() throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public int executeUpdate(String s, int i) throws SQLException {
-        return 0;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public int executeUpdate(String s, int[] ints) throws SQLException {
-        return 0;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public int executeUpdate(String s, String[] strings) throws SQLException {
-        return 0;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public boolean execute(String s, int i) throws SQLException {
-        return false;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public boolean execute(String s, int[] ints) throws SQLException {
-        return false;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public boolean execute(String s, String[] strings) throws SQLException {
-        return false;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public int getResultSetHoldability() throws SQLException {
-        return 0;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public boolean isClosed() throws SQLException {
-        return false;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void setPoolable(boolean b) throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public boolean isPoolable() throws SQLException {
-        return false;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public void closeOnCompletion() throws SQLException {
-
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public boolean isCloseOnCompletion() throws SQLException {
-        return false;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public <T> T unwrap(Class<T> aClass) throws SQLException {
-        return null;
+        throw new SQLException("Feature not implemented");
     }
 
     @Override
     public boolean isWrapperFor(Class<?> aClass) throws SQLException {
-        return false;
+        throw new SQLException("Feature not implemented");
     }
 }
