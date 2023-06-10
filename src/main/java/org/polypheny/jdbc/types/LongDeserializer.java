8dd76e42dba4397858ec19425d1532b5d55c38c5
TobiasHafner <t.hafner@stud.unibas.ch>
2023-06-10 06:42:21 +0200

    add long deserializer

--- src/main/java/org/polypheny/jdbc/types/LongDeserializer.java
+++ src/main/java/org/polypheny/jdbc/types/LongDeserializer.java
@@ -1,12 +1,18 @@
 package org.polypheny.jdbc.types;
 
+import org.polypheny.jdbc.proto.ProtoLong;
 import org.polypheny.jdbc.proto.ProtoValue;
 
 public class LongDeserializer implements ValueDeserializer {
 
     @Override
     public Object deserilize( ProtoValue value ) {
-        return null;
+        return deserializeToLong( value.getLong() );
+    }
+
+
+    private Object deserializeToLong( ProtoLong protoLong ) {
+        return protoLong.getLong();
     }
 
 }
