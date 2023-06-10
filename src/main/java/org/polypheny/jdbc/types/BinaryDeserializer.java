34ced1dd9ed2063684a7477b1b6d25c56c4217bf
TobiasHafner <t.hafner@stud.unibas.ch>
2023-06-10 06:46:45 +0200

    add binary deserializer

--- src/main/java/org/polypheny/jdbc/types/BinaryDeserializer.java
+++ src/main/java/org/polypheny/jdbc/types/BinaryDeserializer.java
@@ -1,12 +1,24 @@
 package org.polypheny.jdbc.types;
 
+import java.sql.Types;
+import org.polypheny.jdbc.proto.ProtoBinary;
 import org.polypheny.jdbc.proto.ProtoValue;
 
 public class BinaryDeserializer implements ValueDeserializer {
 
     @Override
     public Object deserilize( ProtoValue value ) {
-        return null;
+        switch ( ProtoToJdbcTypeMap.getJdbcTypeFromProto( value.getType() ) ) {
+            case Types.BINARY:
+            case Types.VARBINARY:
+                return deserilizeToByteArray( value.getBinary() );
+        }
+        throw new IllegalArgumentException( "Illegal jdbc type for proto string." );
+    }
+
+
+    private Object deserilizeToByteArray( ProtoBinary protoBinary ) {
+        return protoBinary.getBinary().toByteArray();
     }
 
 }
