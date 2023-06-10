2821a2ff121b7853901083ee7c421740943b9a93
TobiasHafner <t.hafner@stud.unibas.ch>
2023-06-10 06:42:58 +0200

    add boolean deserializer

--- src/main/java/org/polypheny/jdbc/types/BooleanDeserializer.java
+++ src/main/java/org/polypheny/jdbc/types/BooleanDeserializer.java
@@ -2,12 +2,8 @@ package org.polypheny.jdbc.types;
 
 import org.polypheny.jdbc.proto.ProtoBoolean;
 import org.polypheny.jdbc.proto.ProtoValue;
-import org.polypheny.jdbc.proto.ProtoValue.ValueCase;
 
 public class BooleanDeserializer implements ValueDeserializer {
-
-    private static final ProtoValue.ValueCase TARGET_CASE = ValueCase.BOOLEAN;
-
 
     @Override
     public Object deserilize( ProtoValue value ) {
