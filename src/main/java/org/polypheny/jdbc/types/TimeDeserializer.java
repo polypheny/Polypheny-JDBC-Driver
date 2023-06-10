9d2f0a5aab6e1b2bc8d442a7b816005093e51031
TobiasHafner <t.hafner@stud.unibas.ch>
2023-06-10 07:02:12 +0200

    add time deserializer

--- src/main/java/org/polypheny/jdbc/types/TimeDeserializer.java
+++ src/main/java/org/polypheny/jdbc/types/TimeDeserializer.java
@@ -1,12 +1,27 @@
 package org.polypheny.jdbc.types;
 
+import java.sql.Time;
+import java.sql.Types;
+import org.apache.commons.lang3.NotImplementedException;
+import org.polypheny.jdbc.proto.ProtoTime;
 import org.polypheny.jdbc.proto.ProtoValue;
 
 public class TimeDeserializer implements ValueDeserializer {
 
     @Override
     public Object deserilize( ProtoValue value ) {
-        return null;
+        switch ( ProtoToJdbcTypeMap.getJdbcTypeFromProto( value.getType() ) ) {
+            case Types.TIME:
+                return deserializeToSqlTime( value.getTime() );
+            case Types.OTHER:
+                throw new NotImplementedException("Deserialization of time with local timezone not implemented yet.");
+        }
+        throw new IllegalArgumentException( "Illegal jdbc type for proto time." );
+    }
+
+
+    private Object deserializeToSqlTime( ProtoTime protoTime ) {
+        return new Time( protoTime.getValue());
     }
 
 }
