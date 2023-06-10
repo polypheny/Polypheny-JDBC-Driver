c479ab5521696628b207e5733295697eda1efa6b
TobiasHafner <t.hafner@stud.unibas.ch>
2023-06-10 07:05:33 +0200

    remove unused method from value deserializer

--- src/main/java/org/polypheny/jdbc/types/ValueDeserializer.java
+++ src/main/java/org/polypheny/jdbc/types/ValueDeserializer.java
@@ -3,8 +3,6 @@ package org.polypheny.jdbc.types;
 import org.polypheny.jdbc.proto.ProtoValue;
 
 public interface ValueDeserializer {
-    boolean deserializes(ProtoValue value);
-
     Object deserilize( ProtoValue value );
 
 }
