59722b488cfafb7bb24c8a67e27fb1ed9b1d5e47
TobiasHafner <t.hafner@stud.unibas.ch>
2023-06-10 06:50:48 +0200

    add date deserializer

--- src/main/java/org/polypheny/jdbc/types/DateDeserializer.java
+++ src/main/java/org/polypheny/jdbc/types/DateDeserializer.java
@@ -1,12 +1,19 @@
 package org.polypheny.jdbc.types;
 
+import java.sql.Date;
+import org.polypheny.jdbc.proto.ProtoDate;
 import org.polypheny.jdbc.proto.ProtoValue;
 
 public class DateDeserializer implements ValueDeserializer {
 
     @Override
     public Object deserilize( ProtoValue value ) {
-        return null;
+        return deserializeToSqlSate(value.getDate());
+    }
+
+
+    private Object deserializeToSqlSate( ProtoDate polyDate ) {
+        return new Date(polyDate.getDate() );
     }
 
 }
