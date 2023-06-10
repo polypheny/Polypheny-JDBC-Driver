0d887e66a12156a929034e49bc678033e6f73703
TobiasHafner <t.hafner@stud.unibas.ch>
2023-06-10 07:05:12 +0200

    add time stamp deserializer

--- src/main/java/org/polypheny/jdbc/types/TimeStampDeserializer.java
+++ src/main/java/org/polypheny/jdbc/types/TimeStampDeserializer.java
@@ -1,12 +1,27 @@
 package org.polypheny.jdbc.types;
 
+import java.sql.Timestamp;
+import java.sql.Types;
+import org.apache.commons.lang3.NotImplementedException;
+import org.polypheny.jdbc.proto.ProtoTimeStamp;
 import org.polypheny.jdbc.proto.ProtoValue;
 
 public class TimeStampDeserializer implements ValueDeserializer {
 
     @Override
     public Object deserilize( ProtoValue value ) {
-        return null;
+        switch ( ProtoToJdbcTypeMap.getJdbcTypeFromProto( value.getType() ) ) {
+            case Types.TIME:
+                return deserializeToSqlTimeStamp( value.getTimeStamp() );
+            case Types.OTHER:
+                throw new NotImplementedException( "Deserialization of timestamps with local timezone not implemented yet." );
+        }
+        throw new IllegalArgumentException( "Illegal jdbc type for proto time." );
+    }
+
+
+    private Object deserializeToSqlTimeStamp( ProtoTimeStamp protoTimeStamp ) {
+        return new Timestamp( protoTimeStamp.getTimeStamp() );
     }
 
 }
