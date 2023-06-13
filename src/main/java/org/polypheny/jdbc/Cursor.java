fea0fd02330515ba8387e489f6af99a234f5a679
TobiasHafner <t.hafner@stud.unibas.ch>
2023-06-13 09:34:39 +0200

    add frame offset to cursor

--- src/main/java/org/polypheny/jdbc/Cursor.java
+++ src/main/java/org/polypheny/jdbc/Cursor.java
@@ -9,7 +9,9 @@ import java.util.NoSuchElementException;
  * https://github.com/polypheny/Avatica/blob/polypheny/core/src/main/java/org/apache/calcite/avatica/util/IteratorCursor.java
  */
 
+
 public class Cursor<T> {
+
     private enum CursorPosition {
         BEFORE_FIRST,
         ON_ELEMENT,
@@ -17,14 +19,26 @@ public class Cursor<T> {
         CLOSED
     }
 
+
     private final Iterator<T> iterator;
+    private int frameOffset;
     private CursorPosition cursorPosition;
     private T currentElement;
+
 
     public Cursor( Iterator<T> iterator ) {
         this.iterator = iterator;
         this.cursorPosition = CursorPosition.BEFORE_FIRST;
+        this.frameOffset = 0;
     }
+
+
+    public Cursor( Iterator<T> iterator, int frameOffset ) {
+        this.iterator = iterator;
+        this.cursorPosition = CursorPosition.BEFORE_FIRST;
+        this.frameOffset = frameOffset;
+    }
+
 
     public boolean next() {
         if ( !iterator.hasNext() ) {
@@ -37,12 +51,14 @@ public class Cursor<T> {
         return true;
     }
 
+
     public T current() {
-        if ( cursorPosition != CursorPosition.ON_ELEMENT) {
-            throw new NoSuchElementException("Cursor must be pointing on an element.");
+        if ( cursorPosition != CursorPosition.ON_ELEMENT ) {
+            throw new NoSuchElementException( "Cursor must be pointing on an element." );
         }
         return currentElement;
     }
+
 
     public void close() {
         currentElement = null;
@@ -50,16 +66,26 @@ public class Cursor<T> {
         closeIterator();
     }
 
+
     private void closeIterator() {
-        if (iterator instanceof AutoCloseable) {
+        if ( iterator instanceof AutoCloseable ) {
             try {
                 ((AutoCloseable) iterator).close();
-            } catch (RuntimeException e) {
+            } catch ( RuntimeException e ) {
                 throw e;
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+            } catch ( Exception e ) {
+                throw new RuntimeException( e );
             }
         }
     }
 
+
+    public boolean isBeforeFirst() {
+        return cursorPosition == CursorPosition.BEFORE_FIRST;
+    }
+
+
+    public boolean isAfterLast() {
+        return cursorPosition == CursorPosition.AFTER_LAST;
+    }
 }
