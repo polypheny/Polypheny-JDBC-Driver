6357c7c04ffd95cbfece207fe0e982d00b047d08
TobiasHafner <t.hafner@stud.unibas.ch>
2023-06-13 08:09:55 +0200

    add database type names

---
+++ src/main/java/org/polypheny/jdbc/types/ProtoToPolyTypeNameMap.java
@@ -0,0 +1,37 @@
+package org.polypheny.jdbc.types;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Arrays;
+import java.util.Map;
+import org.polypheny.jdbc.proto.ProtoValueType;
+
+public class ProtoToPolyTypeNameMap {
+
+    private static final String COMMON_NAME_PREFIX = "PROTO_VALUE_TYPE_";
+    private static final Map<ProtoValueType, String> PROTO_TYPE_TO_POLY_TYPE;
+
+
+    static {
+        PROTO_TYPE_TO_POLY_TYPE = Arrays.stream( ProtoValueType.values() )
+                .sequential()
+                .collect( ImmutableMap.toImmutableMap( n -> n, n -> removeCommonName( n.name() ) ) );
+    }
+
+
+    private static String removeCommonName( String valueName ) {
+        if ( valueName != null && valueName.startsWith( COMMON_NAME_PREFIX ) ) {
+            return valueName.substring( COMMON_NAME_PREFIX.length() );
+        }
+        return valueName;
+    }
+
+
+    public static String getPolyTypeNameFromProto( ProtoValueType protoValueType ) {
+        String polyTypeName = PROTO_TYPE_TO_POLY_TYPE.get( protoValueType );
+        if ( polyTypeName == null ) {
+            throw new IllegalArgumentException( "Invalid proto value type." );
+        }
+        return polyTypeName;
+    }
+
+}
