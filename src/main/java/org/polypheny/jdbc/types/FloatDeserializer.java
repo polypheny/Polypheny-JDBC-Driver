d6d0f3a3ee80b8c97b2d583dc00773c7a5f3290a
TobiasHafner <t.hafner@stud.unibas.ch>
2023-06-13 11:22:55 +0200

    implement  float deserialization

--- src/main/java/org/polypheny/jdbc/types/FloatDeserializer.java
+++ src/main/java/org/polypheny/jdbc/types/FloatDeserializer.java
@@ -1,12 +1,18 @@
 package org.polypheny.jdbc.types;
 
+import org.polypheny.jdbc.proto.ProtoFloat;
 import org.polypheny.jdbc.proto.ProtoValue;
 
 public class FloatDeserializer implements ValueDeserializer {
 
     @Override
     public Object deserilize( ProtoValue value ) {
-        return null;
+        return deserializeToFloat( value.getFloat() );
+    }
+
+
+    private Object deserializeToFloat( ProtoFloat protoFloat ) {
+        return protoFloat.getFloat();
     }
 
 }
