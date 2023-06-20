7129618932822e1100fe3dc8ac83277c210338d9
TobiasHafner <t.hafner@stud.unibas.ch>
2023-06-20 14:44:25 +0200

    add conversion to StatementProperties

--- src/main/java/org/polypheny/jdbc/ConnectionProperties.java
+++ src/main/java/org/polypheny/jdbc/ConnectionProperties.java
@@ -2,6 +2,7 @@ package org.polypheny.jdbc;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.polypheny.jdbc.utils.DefaultPropertyValues;
 
 public class ConnectionProperties {
 
@@ -23,5 +24,35 @@ public class ConnectionProperties {
     @Getter
     @Setter
     private boolean hasRunningTransaction;
+
+
+    public StatementProperties toStatementProperties() {
+        return toStatementProperties(
+                DefaultPropertyValues.getDEFAULT_RESULTSET_TYPE(),
+                DefaultPropertyValues.getDEFAULT_RESULTSET_CONCURRENCY()
+        );
+    }
+
+
+    public StatementProperties toStatementProperties( int resultSetType, int resultSetConcurrency ) {
+        return toStatamentProperties( resultSetType, resultSetConcurrency, resultSetHoldability );
+    }
+
+
+    public StatementProperties toStatamentProperties( int resultSetType, int resultSetConcurrency, int resultSetHoldability ) {
+        StatementProperties properties = new StatementProperties();
+        properties.setQueryTimeoutSeconds( DefaultPropertyValues.getQUERY_TIMEOUT_SECONDS() );
+        properties.setResultSetType( resultSetType );
+        properties.setResultSetConcurrency( resultSetConcurrency );
+        properties.setResultSetHoldability( resultSetHoldability );
+        properties.setFetchSize( DefaultPropertyValues.getFETCH_SIZE() );
+        properties.setFetchDirection( DefaultPropertyValues.getFETCH_DIRECTION() );
+        properties.setMaxFieldSize( DefaultPropertyValues.getMAX_FIELD_SIZE() );
+        properties.setMaxRows( DefaultPropertyValues.getMAX_ROWS() );
+        properties.setLargeMaxRows( DefaultPropertyValues.getLARGE_MAX_ROWS() );
+        properties.setDoesEscapeProcessing( DefaultPropertyValues.isDOING_ESCAPE_PROCESSING() );
+        properties.setPoolable( DefaultPropertyValues.isSTATEMENT_POOLABLE() );
+        return properties;
+    }
 
 }
