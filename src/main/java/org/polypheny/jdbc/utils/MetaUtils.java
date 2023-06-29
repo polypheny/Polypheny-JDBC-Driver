fc986e348306cf9e60dc762a3820ceeb0448e12a
TobiasHafner <t.hafner@stud.unibas.ch>
2023-06-29 17:33:59 +0200

    refactor to meta utils

--- src/main/java/org/polypheny/jdbc/utils/MetaUtils.java
+++ src/main/java/org/polypheny/jdbc/utils/MetaUtils.java
@@ -4,12 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.polypheny.jdbc.PolyphenyColumnMeta;
+import org.polypheny.jdbc.PolyphenyParameterMeta;
 import org.polypheny.jdbc.proto.ColumnMeta;
+import org.polypheny.jdbc.proto.ParameterMeta;
 
-public class ColumnMetaUtils {
+public class MetaUtils {
 
     public static ArrayList<PolyphenyColumnMeta> buildColumnMetas( List<ColumnMeta> protoColumnMetas ) {
         return protoColumnMetas.stream().map( PolyphenyColumnMeta::new ).collect( Collectors.toCollection( ArrayList::new ) );
+    }
+
+    public static ArrayList<PolyphenyParameterMeta> buildParameterMetas(List<ParameterMeta> protoParameterMetas) {
+        return protoParameterMetas.stream().map( PolyphenyParameterMeta::new ).collect( Collectors.toCollection( ArrayList::new ) );
     }
 
 }
