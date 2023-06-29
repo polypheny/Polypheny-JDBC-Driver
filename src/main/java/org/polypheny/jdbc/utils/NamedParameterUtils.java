1805d4d5e8a9954f3221d76e8d7ee93c1451232d
TobiasHafner <t.hafner@stud.unibas.ch>
2023-06-29 17:34:25 +0200

    skeleton for parameter list conversion

---
+++ src/main/java/org/polypheny/jdbc/utils/NamedParameterUtils.java
@@ -0,0 +1,12 @@
+package org.polypheny.jdbc.utils;
+
+import java.util.List;
+import java.util.Map;
+import org.polypheny.jdbc.types.TypedValue;
+
+public class NamedParameterUtils {
+    public static Map<String, TypedValue> convertToParameterMap( List<TypedValue> parameters) {
+        return null;
+    }
+
+}
