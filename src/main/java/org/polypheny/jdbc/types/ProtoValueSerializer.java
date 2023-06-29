9ba6d5cf363f20af028a6f69284c9d7a7c26506e
TobiasHafner <t.hafner@stud.unibas.ch>
2023-06-29 17:53:20 +0200

    add skeleton for parameter map serialization

---
+++ src/main/java/org/polypheny/jdbc/types/ProtoValueSerializer.java
@@ -0,0 +1,12 @@
+package org.polypheny.jdbc.types;
+
+import java.util.Map;
+import org.polypheny.jdbc.proto.ProtoValue;
+
+public class ProtoValueSerializer {
+
+    public static Map<String, ProtoValue> serializeParameterMap( Map<String, TypedValue> parameters ) {
+        return null;
+    }
+
+}
