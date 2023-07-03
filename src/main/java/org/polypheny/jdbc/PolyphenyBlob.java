3d68623fecf72354c1e1d47282fb5d748ac94fbc
TobiasHafner <t.hafner@stud.unibas.ch>
2023-07-03 09:36:57 +0200

    implement basic version of a blob

--- src/main/java/org/polypheny/jdbc/PolyphenyBlob.java
+++ src/main/java/org/polypheny/jdbc/PolyphenyBlob.java
@@ -1,75 +1,152 @@
 package org.polypheny.jdbc;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.sql.Blob;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Arrays;
 
 public class PolyphenyBlob implements Blob {
 
-    @Override
-    public long length() throws SQLException {
-        return 0;
+    /*
+     * This array should be replaced with an objet capable of either storing a collection of bytes larger than MAX_INT
+     * or some kind of streaming mechanism.
+     */
+    byte[] value;
+    boolean isFreed;
+
+
+    public PolyphenyBlob() {
+        this.isFreed = false;
+    }
+
+
+    private long positionToIndex( long position ) {
+        return position - 1;
+    }
+
+
+    private long indexToPosition( long index ) {
+        return index + 1;
+    }
+
+
+    private int longToInt( long value ) {
+        return Math.toIntExact( value );
+    }
+
+
+    private void throwIfPositionOutOfBounds( long position ) throws SQLException {
+        /* jdbc starts enumeration by one */
+        throwIfIndexOutOfBounds( positionToIndex( position ) );
+    }
+
+
+    private void throwIfIndexOutOfBounds( long index ) throws SQLException {
+        if ( index < 0 ) {
+            throw new SQLException( "Index out of bounds" );
+        }
+        if ( index >= value.length ) {
+            throw new SQLException( "Index out of bounds" );
+        }
+    }
+
+
+    private void throwIfFreed() throws SQLException {
+        if ( isFreed ) {
+            throw new SQLException( "Illegal operation on freed blob" );
+        }
     }
 
 
     @Override
-    public byte[] getBytes( long l, int i ) throws SQLException {
-        return new byte[0];
+    public long length() throws SQLException {
+        return value.length;
+    }
+
+
+    @Override
+    public byte[] getBytes( long pos, int length ) throws SQLException {
+        throwIfFreed();
+        throwIfPositionOutOfBounds( pos );
+        throwIfPositionOutOfBounds( pos + length - 1 );
+        return Arrays.copyOfRange( value, longToInt( pos ), length );
     }
 
 
     @Override
     public InputStream getBinaryStream() throws SQLException {
-        return null;
+        throwIfFreed();
+        return new ByteArrayInputStream( value );
     }
 
 
     @Override
-    public long position( byte[] bytes, long l ) throws SQLException {
-        return 0;
+    public long position( byte[] bytes, long start ) throws SQLException {
+        /* Could efficiently be implemented using Knuth-Morris-Pratt-Algorithm */
+        throw new SQLFeatureNotSupportedException("Feature not implemented");
     }
 
 
     @Override
-    public long position( java.sql.Blob blob, long l ) throws SQLException {
-        return 0;
+    public long position( Blob blob, long start ) throws SQLException {
+        throw new SQLFeatureNotSupportedException("Feature not implemented");
     }
 
 
     @Override
-    public int setBytes( long l, byte[] bytes ) throws SQLException {
-        return 0;
+    public int setBytes( long pos, byte[] bytes ) throws SQLException {
+        return setBytes( pos, bytes, 0, bytes.length );
     }
 
 
     @Override
-    public int setBytes( long l, byte[] bytes, int i, int i1 ) throws SQLException {
-        return 0;
+    public int setBytes( long pos, byte[] bytes, int offset, int len ) throws SQLException {
+        throwIfFreed();
+        if ( positionToIndex( pos + len ) >= value.length ) {
+            value = Arrays.copyOf( value, longToInt( positionToIndex( pos + len ) ) );
+        }
+        for ( int bytesWritten = 0; bytesWritten < len; bytesWritten++ ) {
+            int writeIndex = longToInt( positionToIndex( pos ) ) + bytesWritten;
+            value[writeIndex] = bytes[offset + bytesWritten];
+        }
+        return len;
     }
 
 
     @Override
-    public OutputStream setBinaryStream( long l ) throws SQLException {
-        return null;
+    public OutputStream setBinaryStream( long pos ) throws SQLException {
+        throwIfFreed();
+        throw new SQLFeatureNotSupportedException( "Feature not supported" );
     }
 
 
     @Override
-    public void truncate( long l ) throws SQLException {
-
+    public void truncate( long len ) throws SQLException {
+        throwIfFreed();
+        if ( len < 0 ) {
+            throw new SQLException( "Illegal argument for len" );
+        }
+        len = Math.min( len, value.length );
+        value = Arrays.copyOf( value, longToInt( len ) );
     }
 
 
     @Override
     public void free() throws SQLException {
-
+        this.isFreed = true;
     }
 
 
     @Override
-    public InputStream getBinaryStream( long l, long l1 ) throws SQLException {
-        return null;
+    public InputStream getBinaryStream( long pos, long len ) throws SQLException {
+        throwIfFreed();
+        int from = longToInt( positionToIndex( pos ) );
+        int to = longToInt( positionToIndex( pos + len ) );
+        byte[] slice = Arrays.copyOfRange( value, from, to );
+        return new ByteArrayInputStream( slice );
     }
 
 }
