9a07d7cfbe8983aacee7bf48111e1c10646313ab
TobiasHafner <t.hafner@stud.unibas.ch>
2023-07-03 10:33:02 +0200

    implement basic version of a struct

--- src/main/java/org/polypheny/jdbc/types/PolyphenyStruct.java
+++ src/main/java/org/polypheny/jdbc/types/PolyphenyStruct.java
@@ -1,29 +1,29 @@
 package org.polypheny.jdbc.types;
 
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
 import java.sql.Struct;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class PolyphenyStruct implements Struct {
-    List<TypedValue> attributes;
+
+    Map<String, TypedValue> attributes;
+    String typeName;
 
     @Override
     public String getSQLTypeName() throws SQLException {
-        return "";
+        return typeName;
     }
 
 
     @Override
     public Object[] getAttributes() throws SQLException {
-        return attributes.stream().map( TypedValue::asObject ).toArray(Object[]::new);
+        return attributes.values().stream().map( TypedValue::asObject ).toArray();
     }
 
 
     @Override
     public Object[] getAttributes( Map<String, Class<?>> map ) throws SQLException {
-        return new Object[0];
+        throw new SQLFeatureNotSupportedException("Feature not supported");
     }
-
 }
