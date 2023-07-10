4583debb802ab25184e44848f372a4585c5e341a
TobiasHafner <t.hafner@stud.unibas.ch>
2023-07-11 01:31:45 +0200

    add test code for manual debugging

--- src/test/java/org/polypheny/jdbc/QueryTest.java
+++ src/test/java/org/polypheny/jdbc/QueryTest.java
@@ -6,37 +6,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import org.polypheny.jdbc.utils.PropertyUtils;
 
 public class QueryTest {
 
-    @BeforeClass
-    public static void setUpClass() {
-    }
+    public static void main( String[] args ) throws ClassNotFoundException {
 
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-
-    @Before
-    public void setUp() {
-    }
-
-
-    @After
-    public void tearDown() {
-    }
-
-/*
-    @Test
-    public void selectStar__NoError() throws Exception {
         final String DB_URL = "jdbc:polypheny://localhost:20590";
         final String USER = "pa";
         final String PASS = "";
@@ -48,17 +23,23 @@ public class QueryTest {
 
         Class.forName( "org.polypheny.jdbc.PolyphenyDriver" );
 
-        try ( Connection conn = DriverManager.getConnection( DB_URL, CONNECTION_PROPERTIES );
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery( QUERY )
-        ) {
-            while ( rs.next() ) {
-                System.out.print( "ID: " + rs.getInt( "employeeno" ) );
-                System.out.print( ", Age: " + rs.getInt( "age" ) );
+        try {
+            Connection conn = DriverManager.getConnection( DB_URL, CONNECTION_PROPERTIES );
+            Statement stmt = conn.createStatement();
+            ResultSet rs;
+            if ( stmt.execute( QUERY ) ) {
+                rs = stmt.getResultSet();
+                int count = rs.getMetaData().getColumnCount();
+                while ( rs.next() ) {
+                    System.out.print( "ID: " + rs.getInt( "employeeno" ) );
+                    System.out.print( ", Age: " + rs.getInt( "age" ) + "\n" );
+                }
             }
+            int uc = stmt.getUpdateCount();
         } catch ( SQLException e ) {
             e.printStackTrace();
         }
     }
-*/
 }
+
+
