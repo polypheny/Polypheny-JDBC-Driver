941a9422c9f09abbff9efae6637fbdb4ac021e8a
TobiasHafner <t.hafner@stud.unibas.ch>
2023-07-24 21:52:59 +0200

    add protobuf messages for advanced error details

--- src/main/java/org/polypheny/jdbc/ProtoInterfaceServiceException.java
+++ src/main/java/org/polypheny/jdbc/ProtoInterfaceServiceException.java
@@ -1,6 +1,8 @@
 package org.polypheny.jdbc;
 
 import java.sql.SQLException;
+import java.util.Optional;
+import org.polypheny.jdbc.proto.ErrorDetails;
 
 /*
  * Copyright 2019-2023 The Polypheny Project
@@ -18,7 +20,61 @@ import java.sql.SQLException;
  * limitations under the License.
  */
 public class ProtoInterfaceServiceException extends SQLException{
-    public ProtoInterfaceServiceException(String message) {
-        super(message);
+    public ProtoInterfaceServiceException( String reason, String state, int errorCode ) {
+        super( reason, state, errorCode );
+    }
+
+
+    public ProtoInterfaceServiceException( String reason, String state ) {
+        super( reason, state );
+
+    }
+
+
+    public ProtoInterfaceServiceException( String reason ) {
+        super( reason );
+    }
+
+
+    public ProtoInterfaceServiceException() {
+        super();
+    }
+
+
+    public ProtoInterfaceServiceException( Throwable cause ) {
+        super( cause );
+    }
+
+
+    public ProtoInterfaceServiceException( String reason, Throwable cause ) {
+        super( reason, cause );
+    }
+
+
+    public ProtoInterfaceServiceException( String reason, String state, Throwable cause ) {
+        super( reason, state, cause );
+    }
+
+
+    public ProtoInterfaceServiceException( String reason, String state, int errorCode, Throwable cause ) {
+        super( reason, state, errorCode, cause );
+    }
+
+
+    public ProtoInterfaceServiceException( ErrorDetails errorDetails ) {
+        super(
+                errorDetails.hasMessage() ? errorDetails.getMessage() : null,
+                errorDetails.hasState() ? errorDetails.getState() : null,
+                errorDetails.hasErrorCode() ? errorDetails.getErrorCode() : 0
+        );
+    }
+
+
+    public ErrorDetails getProtoErrorDetails() {
+        ErrorDetails.Builder errorDetailsBuilder = ErrorDetails.newBuilder();
+        errorDetailsBuilder.setErrorCode( getErrorCode() );
+        Optional.ofNullable( getSQLState() ).ifPresent( errorDetailsBuilder::setState );
+        Optional.ofNullable( getMessage() ).ifPresent( errorDetailsBuilder::setMessage );
+        return errorDetailsBuilder.build();
     }
 }
