fc2ab70da23e5df1b7d74984e09e54d483a92f20
TobiasHafner <t.hafner@stud.unibas.ch>
2023-08-12 10:47:08 +0200

    remove wrong conversion

--- src/main/java/org/polypheny/jdbc/deserialization/TimeDeserializer.java
+++ src/main/java/org/polypheny/jdbc/deserialization/TimeDeserializer.java
@@ -26,7 +26,7 @@ public class TimeDeserializer implements ValueDeserializer {
 
 
     private Time deserializeToSqlTime( ProtoTime protoTime ) {
-        return new Time( protoTime.getValue() * 86400000L );
+        return new Time( protoTime.getValue() );
     }
 
 }
