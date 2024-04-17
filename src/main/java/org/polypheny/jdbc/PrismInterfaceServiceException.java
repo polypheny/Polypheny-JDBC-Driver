6cd1b3f30ceaf9cb1ca01d8bb52972a966f8830a
Marco Vogt <marco.vogt@unibas.ch>
2024-04-17 09:43:26 +0200

    Update year in copyright

--- src/main/java/org/polypheny/jdbc/PrismInterfaceServiceException.java
+++ src/main/java/org/polypheny/jdbc/PrismInterfaceServiceException.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import org.polypheny.db.protointerface.proto.ErrorDetails;
 
 /*
- * Copyright 2019-2023 The Polypheny Project
+ * Copyright 2019-2024 The Polypheny Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
